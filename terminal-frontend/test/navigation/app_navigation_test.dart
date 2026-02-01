6d7c5e4d218c8151d41e3355b05171e87f8f5f3a
Daniel Gloeckner <gloeckner.daniel@gmail.com>
2026-02-01 23:40:11 +0100

    feat: add navigation integration tests for app flow (Task 13)

---
+++ terminal-frontend/test/navigation/app_navigation_test.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:provider/provider.dart';
+import 'package:ruderbar_terminal/config/app_router.dart';
+import 'package:ruderbar_terminal/providers/auth_provider.dart';
+import 'package:ruderbar_terminal/providers/members_provider.dart';
+import 'package:ruderbar_terminal/providers/products_provider.dart';
+import 'package:ruderbar_terminal/providers/cart_provider.dart';
+import 'package:ruderbar_terminal/providers/sync_provider.dart';
+import 'package:ruderbar_terminal/providers/rfid_provider.dart';
+
+class MockAuthProvider extends Mock implements AuthProvider {}
+class MockMembersProvider extends Mock implements MembersProvider {}
+class MockProductsProvider extends Mock implements ProductsProvider {}
+class MockCartProvider extends Mock implements CartProvider {}
+class MockSyncProvider extends Mock implements SyncProvider {}
+class MockRfidProvider extends Mock implements RfidProvider {}
+
+void main() {
+  group('App Navigation', () {
+    late MockAuthProvider mockAuthProvider;
+    late MockMembersProvider mockMembersProvider;
+    late MockProductsProvider mockProductsProvider;
+    late MockCartProvider mockCartProvider;
+    late MockSyncProvider mockSyncProvider;
+    late MockRfidProvider mockRfidProvider;
+
+    setUp(() {
+      mockAuthProvider = MockAuthProvider();
+      mockMembersProvider = MockMembersProvider();
+      mockProductsProvider = MockProductsProvider();
+      mockCartProvider = MockCartProvider();
+      mockSyncProvider = MockSyncProvider();
+      mockRfidProvider = MockRfidProvider();
+
+      // Setup default mock behaviors
+      when(() => mockAuthProvider.isAuthenticated).thenReturn(true);
+      when(() => mockAuthProvider.addListener(any())).thenReturn(null);
+      when(() => mockAuthProvider.removeListener(any())).thenReturn(null);
+
+      when(() => mockMembersProvider.selectedMember).thenReturn(null);
+      when(() => mockMembersProvider.addListener(any())).thenReturn(null);
+      when(() => mockMembersProvider.removeListener(any())).thenReturn(null);
+
+      when(() => mockProductsProvider.categories).thenReturn([]);
+      when(() => mockProductsProvider.products).thenReturn([]);
+      when(() => mockProductsProvider.addListener(any())).thenReturn(null);
+      when(() => mockProductsProvider.removeListener(any())).thenReturn(null);
+
+      when(() => mockCartProvider.items).thenReturn([]);
+      when(() => mockCartProvider.itemCount).thenReturn(0);
+      when(() => mockCartProvider.addListener(any())).thenReturn(null);
+      when(() => mockCartProvider.removeListener(any())).thenReturn(null);
+
+      when(() => mockSyncProvider.isSyncing).thenReturn(false);
+      when(() => mockSyncProvider.addListener(any())).thenReturn(null);
+      when(() => mockSyncProvider.removeListener(any())).thenReturn(null);
+
+      when(() => mockRfidProvider.addListener(any())).thenReturn(null);
+      when(() => mockRfidProvider.removeListener(any())).thenReturn(null);
+    });
+
+    testWidgets('app starts at idle screen', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MultiProvider(
+          providers: [
+            ChangeNotifierProvider<AuthProvider>.value(value: mockAuthProvider),
+            ChangeNotifierProvider<MembersProvider>.value(value: mockMembersProvider),
+            ChangeNotifierProvider<ProductsProvider>.value(value: mockProductsProvider),
+            ChangeNotifierProvider<CartProvider>.value(value: mockCartProvider),
+            ChangeNotifierProvider<SyncProvider>.value(value: mockSyncProvider),
+            ChangeNotifierProvider<RfidProvider>.value(value: mockRfidProvider),
+          ],
+          child: MaterialApp.router(
+            routerConfig: appRouter,
+          ),
+        ),
+      );
+
+      expect(find.text('Scan Member Card'), findsOneWidget);
+    });
+  });
+}
