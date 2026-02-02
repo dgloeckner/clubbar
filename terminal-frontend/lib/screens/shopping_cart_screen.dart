6c127654180e7569792c8719d215631f4f62f32e
Daniel Gloeckner <gloeckner.daniel@gmail.com>
2026-02-02 19:01:44 +0100

    fix: remove unused database import from ShoppingCartScreen

--- terminal-frontend/lib/screens/shopping_cart_screen.dart
+++ terminal-frontend/lib/screens/shopping_cart_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
-import 'package:ruderbar_terminal/database/database.dart';
 import 'package:ruderbar_terminal/providers/cart_provider.dart';
 import 'package:ruderbar_terminal/utils/design_tokens.dart';
 import 'package:ruderbar_terminal/utils/icon_registry.dart';
